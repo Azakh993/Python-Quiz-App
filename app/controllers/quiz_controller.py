22a08c4b292fc0c95028625bd1cde5662f30bce9
Azmeer K. <azmeer.khawaja@outlook.com>
2023-12-03 00:25:44 +0100

    Finished quiz result controller functions: - Added show_quiz_result function to handle quiz result submission via POST method - Checked for a dashboard request and redirected to the dashboard if requested - Retrieved quiz_id from the session and processed the form to create a dictionary of question_id to selected answers - Utilized correct_quiz function to calculate points and displayed the score in the HTML template - Implemented process_form function to convert form data into a dictionary

--- app/controllers/quiz_controller.py
+++ app/controllers/quiz_controller.py
@@ -1,19 +1,35 @@
 from flask import render_template, redirect, url_for, request, session
 from .controller_helper import validate_login
-from app.services.quiz_service import get_quiz_data
+from app.services.quiz_service import get_quiz_data, correct_quiz
 
 
 def show_quiz_page():
     validate_login()
     quiz_id = session.get("quiz_id")
     subject, questions = get_quiz_data(quiz_id)
-    return render_template("quiz.html", subject=subject, questions=questions)
+    return render_template("quiz.html", points=None, subject=subject, questions=questions)
 
 
 def show_quiz_result():
     if request.method == "POST":
         user_id = validate_login()
-        quiz_id = request.form.get("quiz_id")
 
-        if quiz_id is None:
+        dashboard_request = request.form.get("exit")
+        if dashboard_request is not None:
             return redirect(url_for("show_dashboard_page"))
+
+        quiz_id = session.get('quiz_id')
+        subject, questions = get_quiz_data(quiz_id)
+
+        question_id_to_answer = process_form()
+        points = correct_quiz(user_id, quiz_id, question_id_to_answer)
+
+        return render_template("quiz.html", points=points, subject=subject, questions=questions)
+
+    return show_quiz_result()
+
+
+def process_form():
+    form_dict = request.form.to_dict()
+    question_id_to_answer_dict = {int(key[-1]): value for key, value in form_dict.items()}
+    return question_id_to_answer_dict
