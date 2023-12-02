636f548038377efe41860ecbc16db194a768ea9f
Azmeer K. <azmeer.khawaja@outlook.com>
2023-12-03 00:26:21 +0100

    Implemented result addition or update function: - Defined add_or_update_result function to either add a new result or update an existing one in the database - Retrieved the existing result using user_id and quiz_id - Created a new result if none exists, or updated the existing result with the new score - Committed the changes to the database and added exception handling for errors with rollback and error printing

--- app/repositories/result_repository.py
+++ app/repositories/result_repository.py
@@ -23,3 +23,20 @@ def get_result(username, quiz_id):
         session.rollback()
         raise
 
+
+def add_or_update_result(user_id, quiz_id, new_score):
+    try:
+        result = session.query(Result).filter_by(user_id=user_id, quiz_id=quiz_id).first()
+
+        if result is None:
+            new_result = Result(user_id=user_id, quiz_id=quiz_id, score=new_score)
+            session.add(new_result)
+        else:
+            result.score = new_score
+
+        session.commit()
+
+    except Exception as exception:
+        print(f'Error: {str(exception)}')
+        session.rollback()
+        raise
