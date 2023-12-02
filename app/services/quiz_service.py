e626ef6ae4b6ad84f6df5e53d00874afec800748
Azmeer K. <azmeer.khawaja@outlook.com>
2023-12-03 00:26:00 +0100

    Implemented quiz correction function: - Defined correct_quiz function to compare selected answers with correct answers for each question - Used get_question function to retrieve the correct answer for each question - Incremented correct_answers for each matching answer - Called add_or_update_result function to store the user's result in the database - Returned the number of correct answers

--- app/services/quiz_service.py
+++ app/services/quiz_service.py
@@ -1,6 +1,8 @@
 from app.repositories.quiz_repository import get_quiz
-from app.repositories.question_repository import get_questions
+from app.repositories.question_repository import get_questions, get_question
 import copy
+
+from app.repositories.result_repository import add_or_update_result
 
 
 def process_options(questions):
@@ -17,3 +19,16 @@ def get_quiz_data(quiz_id):
     processed_questions = process_options(questions)
     return subject, processed_questions
 
+
+def correct_quiz(user_id, quiz_id, question_id_to_answer):
+    correct_answers = 0
+    for question_id, answer in question_id_to_answer.items():
+        question = get_question(question_id)
+        correct_answer = question.answer
+
+        if answer == correct_answer:
+            correct_answers += 1
+
+    add_or_update_result(user_id, quiz_id, correct_answers)
+
+    return correct_answers
