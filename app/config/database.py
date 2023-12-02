79271aeb337c9f4e6c615d007ae11164185968e7
Azmeer K. <azmeer.khawaja@outlook.com>
2023-12-02 16:19:21 +0100

    Removed redundant code:
    - Removed the explicit creation of Session and session instances, as they are now created in the app.repository.__init__.py module

--- app/config/database.py
+++ app/config/database.py
@@ -1,5 +1,4 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
 
 DB = 'postgresql'
 DB_USER = 'adm1n'
@@ -10,6 +9,3 @@ DB_PORT = '5432'
 
 sqlalchemy_db_uri = f'{DB}://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}'
 engine = create_engine(sqlalchemy_db_uri)
-
-Session = sessionmaker(bind=engine)
-session = Session()
