3e2a0335e618d2d61b846667a7f11f5e3eef22dd
Azmeer K. <azmeer.khawaja@outlook.com>
2023-12-02 16:17:30 +0100

    Updated dashboard controller to pass data to the HTML template:
    - Called generate_dashboard_data to obtain quizzes_and_results_dict
    - Passed the dictionary as a parameter to the render_template function for rendering in the HTML page

--- app/controllers/dashboard_controller.py
+++ app/controllers/dashboard_controller.py
@@ -1,5 +1,7 @@
 from flask import render_template, session, redirect, url_for
 
+from app.services.dashboard_service import generate_dashboard_data
+
 
 def show_dashboard_page():
     user_id = session.get('user_id')
@@ -7,4 +9,6 @@ def show_dashboard_page():
     if user_id is None:
         return redirect(url_for("show_login_page"))
 
-    return render_template("dashboard.html")
+    quizzes_and_results_dict = generate_dashboard_data(user_id)
+
+    return render_template("dashboard.html", quizzes_and_results_dict=quizzes_and_results_dict)
