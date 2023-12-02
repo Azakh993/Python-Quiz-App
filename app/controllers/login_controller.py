ca5171f3e583262c4bcde847151dc7241e9bb548
Azmeer K. <azmeer.khawaja@outlook.com>
2023-12-02 11:57:29 +0100

    Implemented login functionality: - Route to display login page - Login logic - Sending error message if credentials are incorrect - Redirect to dashboard on successful login

---
+++ app/controllers/login_controller.py
@@ -0,0 +1,22 @@
+from flask import render_template, request, url_for, redirect, session
+from app.services.authentication_service import authenticate_user
+
+
+def show_login_page():
+    return render_template("login.html")
+
+
+def login():
+    if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
+
+        user = authenticate_user(username, password)
+
+        if user is None:
+            return render_template("login.html", error="Invalid username or password")
+
+        session['user_id'] = user.id
+        return redirect(url_for("show_dashboard_page"))
+
+    return show_login_page()
