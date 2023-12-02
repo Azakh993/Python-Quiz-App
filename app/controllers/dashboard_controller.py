8fee25b2f4566d0123fd59ee9b82aef12bcc5489
Azmeer K. <azmeer.khawaja@outlook.com>
2023-12-02 21:59:03 +0100

    Refactored dashboard controller functions: - Incorporated login validation using the validate_login function from controller_helper - Modified show_dashboard_page to retrieve and pass dashboard data to the HTML template - Implemented select_quiz function for handling quiz selection via POST method

--- app/controllers/dashboard_controller.py
+++ app/controllers/dashboard_controller.py
@@ -1,14 +1,18 @@
-from flask import render_template, session, redirect, url_for
-
+from flask import render_template, redirect, url_for, request, session
+from .controller_helper import validate_login
 from app.services.dashboard_service import generate_dashboard_data
 
 
 def show_dashboard_page():
-    user_id = session.get('user_id')
-
-    if user_id is None:
-        return redirect(url_for("show_login_page"))
-
+    user_id = validate_login()
     quizzes_and_results_dict = generate_dashboard_data(user_id)
-
     return render_template("dashboard.html", quizzes_and_results_dict=quizzes_and_results_dict)
+
+
+def select_quiz():
+    if request.method == "POST":
+        validate_login()
+        quiz_id = request.form.get("quiz_id")
+        session["quiz_id"] = quiz_id
+        if quiz_id is not None:
+            return redirect(url_for("show_quiz_page"))
