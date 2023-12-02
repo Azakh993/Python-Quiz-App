4ca490e93aec46a4eb69361c3a2f306ea22ae1c8
Azmeer K. <azmeer.khawaja@outlook.com>
2023-12-02 23:23:25 +0100

    Enhanced quiz service functions: - Imported copy module for deep copying - Modified process_options to use deep copy and avoid modifying the original questions - Returned the processed_questions for clarity

--- app/services/quiz_service.py
+++ app/services/quiz_service.py
@@ -1,16 +1,19 @@
 from app.repositories.quiz_repository import get_quiz
 from app.repositories.question_repository import get_questions
+import copy
 
 
 def process_options(questions):
-    for question in questions:
+    questions_copy = copy.deepcopy(questions)
+    for question in questions_copy:
         split_options = question.options.split(',')
         question.options = split_options
+    return questions_copy
 
 
 def get_quiz_data(quiz_id):
     subject = get_quiz(quiz_id).subject
     questions = get_questions(quiz_id)
-    process_options(questions)
-    return subject, questions
+    processed_questions = process_options(questions)
+    return subject, processed_questions
 
